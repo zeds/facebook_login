eb52eb88781a40a23c1d51ea6d5f8450e40b8306
zeds <tom.zed39@gmail.com>
2019-02-08 11:22:53 +0700

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,23 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+
+  # mailer setting
+  # default url
+    config.action_mailer.default_url_options = {  host: 'localhost', port: 3000 }
+    # mail setting
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :user_name => "tom.zed39@gmail.com",
+      :password => "hoikoi39",
+      :authentication => :plain,
+      :enable_starttls_auto => true
+    }
+
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
