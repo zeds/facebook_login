b151d453a0bf1b9566f6e652261fa35823f559ad
zeds <tom.zed39@gmail.com>
2019-03-02 06:54:19 +0700

    show buy button

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,16 @@ class PostsController < ApplicationController
   end
 
   def show
-    id = params['id']
-    pr_code = params['pr_code']
 
-    @detail = SlornApis.new.get_product_detail(id)
+    if params['id'] != nil
+      $id = params['id']
+    end
+
+    if params['pr_code'] != nil
+      $pr_code = params['pr_code']
+    end
+
+    @detail = SlornApis.new.get_product_detail($id)
 
     if @detail['ticket_billing'] == '都度課金'
       @detail['ticket_billing'] = '<div hidden></div>'
@@ -14,28 +20,42 @@ class PostsController < ApplicationController
       @detail['ticket_billing'] = "<br/>このチケットは" + @detail['ticket_billing'] + "となります。"
     end
 
+    customer_id = "99999"
+    email = "hoge@example.com"
+
+    if current_user != nil
+      customer_id = current_user.customer_id
+      email = current_user.email
+    end
+
     @aid = "116389"
     @pt = "1"
     @jb = "CAPTURE"
     @cmd = "2"
-    @cod = current_user.customer_id
-    @customer_id = current_user.customer_id
-    @iid = pr_code
-    @pr_code = pr_code
-    @em = current_user.email
+    @cod = customer_id
+    @customer_id = customer_id
+    @iid = $pr_code
+    @pr_code = $pr_code
+    @em = email
+
+    if user_signed_in?
+      # ログインしている時は、購入ボタンを消し、postのボタンを表示する
+      @show_buy_button = false
+    else
+      @show_buy_button = true
+    end
 
   end
 
   #購入
   def create
-
     if user_signed_in?
-      flash[:notice] = "customer_id:" + current_user.customer_id.to_s + ",name:" + current_user.name
-      @res = SlornApis.new.post_robot_payment(1,2)
-
+      #購入ボタンを隠す
+      @show_buy_button = true
       redirect_to posts_show_path
     else
-      flash[:notice] = "ログインしてください"
+      @show_buy_button = false
+      flash[:notice] = "ログインまたは新規登録してください"
       redirect_to posts_show_path
     end
   end
